00a8f11e894e36d22b9c13f99ecbfeb3bf925c44
PedrinhoTremBala <rm95435@estudante.fieb.edu.br>
2025-04-04 09:55:50 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
+import poke from './assets/pokeapi_256.png'
  
 function App() {
  
-  const [nome, setNome] = useState("Memphis")
- 
-  // var nome = "pedrinho";
+const Formulario = (props)=>{
+const [nome, setNome] = useState("Memphis")
+
  
   return (
     <div>
-      <h3>Pizzaria 2F</h3>
+      <h3></h3>
+
+
       <input
       className="nome"
       name = "nome"
-      onChange={(e)=>{nome=e.target.value}}
-      placeholder="Digite um nome..."
+      onChange={(e)=>{setName(e.target.value)}}
+      placeholder={props.sombra}
       type="text" />
+
+
+
       <button
       className="botao"
       onClick={()=>{alert(nome)}}>
@@ -24,5 +30,17 @@ function App() {
   )
 }
  
+return (
+
+
+<div>
+  <h3>Pizzaria 2F</h3>
+  <img src = {poke} style={{width:500, height:200}}/>
+    <Formulario sombra="Digite seu nome..."/>
+    <Formulario sombra="Digite seu email..."/>
+    <Formulario sombra=""/>
+</div>
+)
+}
+
 export default App
- 
